da8a59ed7c95b0dbd21190d28c0b8e44f1cbe150
kierangoodacre <k.goodacre@hotmail.co.uk>
2015-01-13 18:54:53 +0000

    created link and storing of links test and passed

--- lib/server.rb
+++ lib/server.rb
@@ -23,4 +23,11 @@ class BookmarkManager < Sinatra::Base
 		erb :index
 	end
 
+	post '/links' do
+	  url = params["url"]
+	  title = params["title"]
+	  Link.create(:url => url, :title => title)
+	  redirect to('/')
+	end
+
 end
