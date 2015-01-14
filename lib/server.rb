49d22fa7884ca328ce3303defad91b8dc4230c51
kierangoodacre <k.goodacre@hotmail.co.uk>
2015-01-14 09:52:14 +0000

    extended user class to allow for passwords

--- lib/server.rb
+++ lib/server.rb
@@ -40,4 +40,14 @@ class BookmarkManager < Sinatra::Base
 	  erb :index
 	end
 
+	get '/users/new' do
+		erb :"users/new"
+	end
+
+	get '/users' do
+		User.create(:email=> params[:email],
+								:password => params[:password])
+		redirect to('/')
+	end
+
 end
