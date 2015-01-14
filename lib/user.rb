7b32a447948a9b1b619200c6cbce2d7107af8246
kierangoodacre <k.goodacre@hotmail.co.uk>
2015-01-14 16:51:22 +0000

    changed error message

--- lib/user.rb
+++ lib/user.rb
@@ -6,8 +6,9 @@ class User
 	include DataMapper::Resource
 
 	property :id, Serial
-	property :email, String
+	property :email, String, :unique => true, :message => "This email is already taken"
 	property :password_digest, Text
+
 
 	attr_reader :password
 	attr_accessor :password_confirmation
